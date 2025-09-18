a3bd8b3e8de64d03295cf200d4eb50547950bc20
ficctProjects <Devbonillas@gmail.com>
2025-09-18 19:14:07 -0400

    render sube requiremetns por mi

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -o erreixt
 
-# poetry install
-install -r requirements.txt
-
+#  poetry install
+# install -r requirements.txt
+# pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
